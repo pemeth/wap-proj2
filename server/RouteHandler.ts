be9b2dfb0274477aeddcd5f9d41505c42ffbe8fa
Mathew Škuta <skuta.matus@gmail.com>
2021-04-15 04:01:21 +0200

    Before merge commit

--- server/RouteHandler.ts
+++ server/RouteHandler.ts
@@ -21,7 +21,7 @@ export class RouteHandler {
                 return resolve();
             })
             .catch(() => {
-                return resolve();
+                return reject();
             });
         });
     }
