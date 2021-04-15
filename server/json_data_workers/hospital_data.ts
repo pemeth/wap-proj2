b11429760f17f7a5c4eccde52ddf7215c48999a4
Oramaramerdman <patriknemethpn@gmail.com>
2021-04-15 14:49:28 +0200

    Change format of sent JSON for hospital bed occupancies
    The outgoing JSON objects now contain only three properties: country,
    date, value. The rest were not important, so they could be dropped.

--- server/json_data_workers/hospital_data.ts
+++ server/json_data_workers/hospital_data.ts
@@ -38,53 +38,65 @@ export class HospitalDataWorker {
     public getDataByCountry(country_code: string): Promise<HospitalDatas> {
         return new Promise<HospitalDatas>((resolve, reject) => {
             const country: string | null = this.countryCodeToCountry(country_code);
-            
+
             // Country not found
             if (country === null) {
                 return reject();
             }
-            
-            return resolve(this.data.filter((data: HospitalData) => {
+
+            var filtered: HospitalDatas = this.data.filter((data: HospitalData) => {
                 return data.country && data.country.toLowerCase() === country;
-            }));
+            });
+
+            filtered = this.convertToOutputFormat(filtered);
+
+            return resolve(filtered);
         });
     }
 
     public getDataByCountryDate(country_code: string, date: Date): Promise<HospitalDatas> {
         return new Promise<HospitalDatas>((resolve, reject) => {
             const country: string | null = this.countryCodeToCountry(country_code);
-                
+
             // Country not found
             if (country === null) {
                 return reject();
             }
-    
-            return resolve(this.data.filter((data: HospitalData) => {
+
+            var filtered: HospitalDatas = this.data.filter((data: HospitalData) => {
                 return data.country !== undefined && data.date && data.country.toLowerCase() === country && (new Date(data.date)).getTime() === date.getTime();
-            }));
+            });
+
+            filtered = this.convertToOutputFormat(filtered);
+
+            return resolve(filtered);
         });
     }
 
     public getDataByCountryDateFromTo(country_code: string, date_from: Date, date_to: Date): Promise<HospitalDatas> {
         return new Promise<HospitalDatas>((resolve, reject) => {
             const country: string | null = this.countryCodeToCountry(country_code);
-                
+
             // Country not found
             if (country === null) {
                 return reject();
             }
-    
-            return resolve(this.data.filter((data: HospitalData) => {
+
+            var filtered: HospitalDatas = this.data.filter((data: HospitalData) => {
                 if (data.date === undefined) {
                     return false;
                 }
-    
+
                 const date: number = new Date(data.date).getTime();
-    
+
                 return data.country && data.date && data.country.toLowerCase() === country &&
                 date_from.getTime() <= date &&
                 date <= date_to.getTime();
-            }));
+            });
+
+            filtered = this.convertToOutputFormat(filtered);
+
+            return resolve(filtered);
         });
     }
 
@@ -99,5 +111,25 @@ export class HospitalDataWorker {
 
         // Invalid country code
         return null;
+    }
+
+    /**
+     * Takes parsed hospital occupancy data and converts it to a
+     * unified output format. The output objects
+     * contain the `country`, `date` and `value` properties.
+     *
+     * @param data Input data to be converted to predefined format.
+     * @returns The same data except in the correct output format.
+     */
+    private convertToOutputFormat(data: HospitalDatas): HospitalDatas {
+        var newdata: HospitalDatas = [];
+        for (const item of data) {
+            newdata.push({
+                country: item.country,
+                date: item.date,
+                value: item.value});
+        }
+
+        return newdata;
     }
 }
