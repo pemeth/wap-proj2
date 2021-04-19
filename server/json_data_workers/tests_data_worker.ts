4a9abf4ca6946bba9709b58942d35a29a38f21a9
Oramaramerdman <patriknemethpn@gmail.com>
2021-04-19 16:54:12 +0200

    Add JSDoc @class tag to class

--- server/json_data_workers/tests_data_worker.ts
+++ server/json_data_workers/tests_data_worker.ts
@@ -14,6 +14,7 @@ import { IncomingMessage } from "node:http";
 
 /**
  * Class holding loaded covid tests JSON data, that will filter out and return data based on given conditions
+ * @class
  */
 @singleton()
 export class TestsDataWorker {
